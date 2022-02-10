0ef858b2fd4a0d42fd230334e7467a3031602401
Marianne54 <69980353+Marianne54@users.noreply.github.com>
2022-02-10 10:46:48 +0100

    Add files via upload

--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,19 +1,42 @@
+import 'package:todo_list/components/tasks/task_details.dart';
+import 'package:todo_list/models/task.dart';
+import 'package:todo_list/screens/create_task.dart';
 import 'package:flutter/material.dart';
+import '../data/tasks.dart';
+import '../models/task.dart';
+
+import '../components/tasks/task_master.dart';
 
 
 class AllTasks extends StatefulWidget {
   const AllTasks({Key? key}) : super(key: key);
-
   @override
   _AllTasksState createState() => _AllTasksState();
 }
 
 class _AllTasksState extends State<AllTasks> {
-  void _addTask() {
-    print('test');
-  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('to do list'),), body: Center(child: Text('test'),), floatingActionButton: FloatingActionButton(onPressed: _addTask, tooltip: 'addTask', child: const Icon(Icons.add),),);
+
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Liste des tâches")),
+        body: TaskMaster(taskslist: Tasks.getTasks()),
+      ),
+    );
   }
+
+
+  List<Task> getItems(){
+    final List<Task> _toDoWidget = <Task>[];
+    for(Task task in Tasks.getTasks()){
+      _toDoWidget.add(task);
+    }
+
+    return _toDoWidget;
+  }
+}
+
+class GoogleFonts {
 }
