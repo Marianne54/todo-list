70e30f2f02161972ab7d3673ff6a936db89dfb5a
Marianne54 <69980353+Marianne54@users.noreply.github.com>
2022-02-10 10:33:58 +0100

    Add files via upload

---
+++ lib/data/tasks.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/models/task.dart';
+import 'package:faker/faker.dart';
+
+class Tasks{
+
+  static List<Task> getTasks(){
+    var faker = new Faker();
+    return List<Task>.generate(15, (int i) => Task(i, faker.food.restaurant(), i%2 == 0 ? true : false) );
+
+  }
+
+}
