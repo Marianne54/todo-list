65432c4f1d9a2476bbf01f7de21169d66b0af84e
Marianne54 <69980353+Marianne54@users.noreply.github.com>
2022-02-10 10:42:35 +0100

    Add files via upload

---
+++ lib/components/tasks/task_details.dart
@@ -0,0 +1,25 @@
+// TODO Implement this library.
+import 'package:todo_list/models/task.dart';
+import 'package:flutter/material.dart';
+
+class TaskDetails extends StatefulWidget {
+
+  final Task task;
+  TaskDetails(this.task);
+
+
+  @override
+  _TaskDetailsState createState() => _TaskDetailsState();
+}
+
+class _TaskDetailsState extends State<TaskDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.task.toString())
+      ),
+      body: TaskDetails(widget.task),
+    );
+  }
+}
---
+++ lib/components/tasks/task_master.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TaskMaster extends StatelessWidget {
+  const TaskMaster({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TASK master'),
+      ),
+    );
+  }
+}
